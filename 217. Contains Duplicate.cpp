7a6cd50d7d794d9efea370977d58d0dfa636c19a
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-06 17:35:34 +0530

    217. Contains Duplicate

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+//217. Contains Duplicate
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> m;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            m[nums[i]]++;
+        }
+        for(auto x: m)
+        {
+            if(x.second>1)
+                return true;
+        }
+        return false;
+    }
+};
